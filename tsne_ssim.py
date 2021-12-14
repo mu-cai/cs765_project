7c189575a7dbc10053a148f7f021f881e6ce98c6
mu-cai <im.mucai@gmail.com>
2021-12-14 00:02:48 -0600

    x

--- tsne_ssim.py
+++ tsne_ssim.py
@@ -31,7 +31,7 @@ if high_score == 'LPIPS':
 index1 = 1
 index2 = 2
 scale = 255
-sample_per_class = 1 # 200 #  200
+sample_per_class = 50 # 200 #  200
 deep_input_size = 32
 num_neighbors = 3
 show_image = False #  True # False
