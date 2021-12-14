c5a0ce4973a35461131da77859b2feeff9cbfbe6
mu-cai <im.mucai@gmail.com>
2021-12-14 15:55:24 -0600

    x

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-sample_per_class_list=(10 50 1)
+# sample_per_class_list=(10 50 1)
 for low_score in UMAP TSNE
 do
     {
     for dataset in mnist cifar
     do
         {
-            for sample_per_class in sample_per_class_list
+            for sample_per_class in 10 50 1
             do
                 {
                     python -W ignore tsne_ssim.py --low_score $low_score --dataset $dataset --sample_per_class $sample_per_class
